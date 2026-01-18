be82c6c1b2dc69b9e2369de77c6e5b6c8d2aee1b
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2026-01-18 15:23:45 +0530

    ui update #10

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,14 +1,7 @@
-import Header from "@/app/components/Header";
-import Background from "@/app/components/Background";
-export default function Dashborad(){
-    return (
-        <>
-            <div className="relative h-screen w-screen overflow-hidden bg-background justify-items-center">
-        <Header stay={true}/>
-                <Background>
-
-                </Background>
-            </div>
-        </>
-)
+export default function Dashborad() {
+  return (
+    <>
+      <div className="relative h-screen w-screen overflow-hidden bg-background justify-items-center"></div>
+    </>
+  );
 }
