5b73fc67e9f28ee8a303123b04a32083f93c68e3
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2026-01-18 18:30:36 +0530

    ui update #16

--- app/page.tsx
+++ app/page.tsx
@@ -84,6 +84,28 @@ export default function Home() {
 
   return (
     <div className="w-full bg-background overflow-x-hidden">
+      <header className="h-12 w-11/12 absolute border-b-2 border-b-[#003D5C] justify-self-center justify-between flex items-center px-4 z-40">
+        <p className="text-[#97ACC7] font-sofia italic ">ANTS WEEKLY</p>
+        <span
+          className="flex flex-row gap-1 items-center "
+          onClick={() => {
+            router.push("/login");
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="20px"
+            fill="#97ACC7"
+          >
+            <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
+          </svg>
+          <p className="text-[#97ACC7] font-sofia italic font-light hover:text-[#97ACC7]/40 transition-colors duration-250 cursor-default">
+            login
+          </p>
+        </span>
+      </header>
       <section className="relative h-screen w-full flex items-center justify-center md:justify-start">
         <SvgBackground_hero height={1400} width={980} />
         <div className="relative z-10 w-full max-w-7xl mx-auto px-6 md:px-24 flex flex-col items-center md:items-start text-center md:text-left gap-2">
@@ -162,7 +184,7 @@ export default function Home() {
                   ["Amol Desai", "Co-founder / Editor / Designer"],
                   ["Sheshadrinivas Chekuri", "Developer / Designer"],
                   ["Praansh Sanghvi", "Designer"],
-                  ["Aarvi pandey", "Editor"],
+                  ["Aarvi Pandey", "Editor"],
                   ["Viaan Lohia", "Scout"],
                   ["Stephen Everett", "Scout"],
                   ["Meit Saboo", "Scout"],
