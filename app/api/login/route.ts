6021554ec6bac6498cae002f7ad5e771ea4e000c
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2026-02-15 22:22:24 +0530

    update

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -6,7 +6,6 @@ import { cookies } from "next/headers";
 export async function POST(request: Request) {
   try {
     const { username, password } = await request.json();
-
     const { data, error } = await supabase
       .from("user_auth")
       .select("password")
@@ -18,27 +17,28 @@ export async function POST(request: Request) {
     }
 
     const isValid = await argon2.verify(data.password, password);
-    const  isAdmin = await  supabase
+    const isAdmin = await supabase
       .from("roles")
       .select("role")
       .eq("name", username)
       .single();
     const cookieStore = await cookies();
     if (isValid) {
-        if( isAdmin.data?.role === 'admin') {
-        cookieStore.set("admin_session", "logged-in", { httpOnly: true,
-            secure: true,
-            sameSite: "strict",
-            path: "/",
+      if (isAdmin.data?.role === "admin") {
+        cookieStore.set("admin_session", "logged-in", {
+          httpOnly: true,
+          secure: true,
+          sameSite: "strict",
+          path: "/",
         });
-        }
-        else {
-      cookieStore.set("session", "logged-in", {
-        httpOnly: true,
-        secure: true,
-        sameSite: "strict",
-        path: "/",
-      });}
+      } else {
+        cookieStore.set("session", "logged-in", {
+          httpOnly: true,
+          secure: true,
+          sameSite: "strict",
+          path: "/",
+        });
+      }
     }
 
     return NextResponse.json({ auth: isValid });
